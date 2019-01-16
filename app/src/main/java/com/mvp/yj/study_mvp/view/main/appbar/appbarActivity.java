f68dcbaf5e985e53eb861927a0b11fb134834a40
YoungK <quarl894@gmail.com>
2019-01-16 22:48:35 +0900

    bottom appbar test

--- app/src/main/java/com/mvp/yj/study_mvp/view/main/appbar/appbarActivity.java
+++ app/src/main/java/com/mvp/yj/study_mvp/view/main/appbar/appbarActivity.java
@@ -1,6 +1,9 @@
 package com.mvp.yj.study_mvp.view.main.appbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
@@ -9,9 +12,12 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.mvp.yj.study_mvp.R;
 import com.mvp.yj.study_mvp.view.main.base.baseActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.zip.Inflater;
+
+import androidx.appcompat.widget.Toolbar;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class appbarActivity extends baseActivity {
     @BindView(R.id.fab)
@@ -19,13 +25,13 @@ public class appbarActivity extends baseActivity {
     @BindView(R.id.bottom_app_bar)
     BottomAppBar bar;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.bottom_appbar);
-
+        setContentView(R.layout.activity_appbar);
         ButterKnife.bind(this);
+
+        init();
 
         fbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +39,25 @@ public class appbarActivity extends baseActivity {
                 ShowToast("click the fab");
             }
         });
+    }
 
+
+    public void init(){
+        bar.replaceMenu(R.menu.appbar_menu);
+        bar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.app_bar_fav :
+                        ShowToast("item1");
+                        break;
+                    case R.id.app_bar_search :
+                        ShowToast("search");
+                        break;
+                }
+                return true;
+
+            }
+        });
     }
 }
